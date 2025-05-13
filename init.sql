048fe49348f03158ce12c4ceeae746fe8692b357
Lingly70 <manshenhehe@gmail.com>
2025-05-13 18:23:46 -0400

    JDBC works

--- init.sql
+++
@@ -1,9 +0,0 @@
-
-CREATE TABLE liquor (
-  id             SERIAL PRIMARY KEY,
-  name           VARCHAR(255) NOT NULL,
-  container_type VARCHAR(20) NOT NULL
-                   CHECK (container_type IN ('pack','bottle','can')),
-  abv            NUMERIC(4,2),              -- e.g. 40.00 for 40% ABV
-  quantity       INTEGER    NOT NULL DEFAULT 0
-);
