6077d32362c322d1fbf2dafa3daf96f3ea4767b7
Lingly70 <manshenhehe@gmail.com>
2025-05-13 18:13:34 -0400

    Update App.java for SQL query testing"

--- src/main/java/com/example/liquor/App.java
+++ src/main/java/com/example/liquor/App.java
@@ -1,13 +1,50 @@
 package com.example.liquor;
 
-/**
- * Hello world!
- *
- */
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
 public class App 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        //initialize credentials to connect to postgresql dbms.
+
+        String url = "jdbc:postgresql://localhost:5432/liquorstore";
+        String user = "app_user";
+        String password = System.getenv("DB_PASS"); // password saved on personal device's env variable. to be changed later(?)
+
+        try {
+
+            //load postgresql driver class into the jvm so we can proceed with the connection+query in the next try
+            Class.forName("org.postgresql.Driver");
+            
+        } catch (ClassNotFoundException e) {
+            System.err.print("Postgres JDBC driver not FOUND");
+            return;
+        }
+
+        try (Connection c = DriverManager.getConnection(url, user, password)) {
+
+            System.out.println("works.");
+            String query = "SELECT * FROM liquor";
+
+
+            try(PreparedStatement ps = c.prepareStatement(query); ResultSet rs = ps.executeQuery()){
+
+                while(rs.next()) {
+
+                    int id = rs.getInt("id");
+
+                    System.out.printf("%d, %n", id);
+                }
+            }
+
+            
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
     }
 }
---
+++ testdel.sql
@@ -0,0 +1,15 @@
+DELETE FROM liquor
+WHERE name IN (
+  'Smirnoff Vodka',
+  'Jameson Irish Whiskey',
+  'Jack Daniels Tennessee Rye',
+  'Jose Cuervo Silver Tequila',
+  'Captain Morgan Spiced Rum',
+  'Budweiser',
+  'Coors Light',
+  'Corona Extra',
+  'Heineken Lager',
+  'Guinness Draught',
+  'White Claw Black Cherry',
+  'Modelo Especial'
+);
