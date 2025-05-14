c01510a0474d02f3555a5f81b8b96aaab1cb2723
Lingly70 <manshenhehe@gmail.com>
2025-05-14 11:37:11 -0400

    Testing resources folder

--- src/main/java/com/example/liquor/LiquorSearchApplication.java
+++ src/main/java/com/example/liquor/LiquorSearchApplication.java
@@ -3,12 +3,11 @@ package com.example.liquor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import com.sun.tools.javac.Main;
 
+@SpringBootApplication
 public class LiquorSearchApplication {
 
     
-    @SpringBootApplication
     public static void main(String[] args){
         SpringApplication.run(LiquorSearchApplication.class, args);
 
